aa720f5098b6dcbe091a86f6ac8a52957fb79b47
Shad Adu <sxantwi@gmail.com>
2015-10-14 16:50:12 -0400

    nytimes scrape

--- Scrapey.py
+++ Scrapey.py
@@ -1,38 +1,73 @@
+__author__ = 'shadrack'
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://www.nypost.com/news/"
-r = requests.get(url)
-soup = BeautifulSoup(r.content,"lxml")
+urls_list = {"nypost":"http://www.nypost.com/news/", "nytimes":"http://international.nytimes.com"}
+#urls_list = {"nypost":"http://www.nypost.com/news/"}
+dates_tags = {"nypost":"div", "nytimes":"time"}
+dates_class = {"nypost":"entry-meta", "nytimes":"timestamp"}
+summary_tags = {"nypost":"div", "nytimes":"p"}
+summary_class = {"nypost":"entry-content", "nytimes":"summary"}
 
-TempList = []
-date_data = soup.find_all("div", {"class":"entry-meta"})
+for dict in urls_list:
+    url=urls_list[dict]
+    r = requests.get(url)
+    #soup = BeautifulSoup(r.content,"lxml")
+    soup = BeautifulSoup(r.content,"html.parser")
+    TempList = []
+    date_data = soup.find_all(dates_tags[dict], {"class":dates_class[dict]})
 
-for item in date_data:
-	dateString = (item.text).replace("Published:","")
-	TempList.append((dateString).strip())
-	
+    # site-specific parsings
+    if dict == "nytimes":
+        currentdate = soup.find_all("li",{"class":"date"})
+        a = str(currentdate)
+        curr_date = a[2:][1+a[2:].find('>'):a[2:].find('<')]
 
-TempList2 = []
-news_data = soup.find_all("div", {"class":"entry-content"})
+    for item in date_data:
+        print item.text
+        if dict == "nypost":
+            dateString = (item.text).replace('Published:',"")
+        else:
+            dateString = item.text
 
-for item in news_data:
-	TempList2.append((item.text).strip())
-	
+        if dict == "nytimes":
+            dateString = curr_date + "| " + dateString
 
-HeadLines = []
-for count in xrange(len(TempList)):
-	HeadLines.append([TempList[count],TempList2[count]])
+        TempList.append((dateString).strip())
 
-for count in xrange(len(HeadLines)):
-	print HeadLines[count][0],
-	print ": ",
-	print HeadLines[count][1],
-	
-	
-	
-   
-    
+    ## end site-specific parsings
+
+    TempList2 = []
+    news_data = soup.find_all(summary_tags[dict], {"class":summary_class[dict]})
+
+    for item in news_data:
+        TempList2.append((item.text).strip())
+
+
+    HeadLines = []
+    for count in xrange(len(TempList)):
+        HeadLines.append([TempList[count],TempList2[count]])
+
+
+    for count in xrange(len(HeadLines)):
+        print HeadLines[count][0],
+        print ": ",
+        print HeadLines[count][1],
+        print '\n'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
